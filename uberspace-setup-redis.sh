00a734efc1053741bf32a7ed35a0602feeed5f2f
Daniel Heitmann <dictvm@lynx.uberspace.de>
2012-11-29 20:37:55 +0100

    Add initial setup-script in untested state

--- uberspace-setup-redis.sh
+++ uberspace-setup-redis.sh
@@ -1,50 +1,66 @@
+#!/bin/bash
+
+USER=`id -u`
+
+if ["$USER" = "0" ] ; then
+  echo "This script is not meant to be run as root."
+  exit 1
+fi
+
+if [ ! -d ~/service ] ; then
+  echo "You don't have a ~/service directory."
+  echo "Please run \"uberspace-setup-svscan\" first to initialize daemon-tools before setting up Redis."
+  exit 1
+fi
+
+# Let's build redis
+echo "Installing redis. This might take some time."
+toast arm redis
+
+if ["$?" ! = "0" ] then
+  echo "Redis-installation with toast failed. This should not have happend."
+  echo "Please contact hallo@uberspace.de so we can fix this. Thanks!"
+  echo "Redis could not be installed. We're really sorry."
+  exit 1
+fi
+
+# create configuration for redis
+echo "Creating Redis-configuration"
+mkdir .redis
+cat <<__EOF__ > unixsocket /home/$USER/.redis/sock
+daemonize no
+logfile /home/$USER/.redis/log
+port 0
+__EOF__
+
+mkdir ~/etc/run-redis
+cat <<__EOF__ > ~/etc/run-redis/run
 #!/bin/sh
+exec redis-server ~/.redis/conf
+__EOF__
+chmod 755 ~/etc/run-redis/run
 
-usage() {
-  echo "Usage: $0 <service to be removed>"
-  echo "for example: $0 mydaemon"
-  exit 1
-}
+echo "Creating symlink for ~/etc/run-redis to ~/service/redis to start the service"
+ln -s ../etc/run-redis ~/service/redis
 
-if [ $# -ne 1 ]
-then
-  echo "One argument expected."
-  echo "You probably forgot to name the daemon you want to expel?"
-  exit 1
+echo "Starting Redis..."
+svc -u ~/service/redis
+
+if ["$?" ! = "0" ] then
+  echo "Redis couldn't be started. This should not have happened."
+  echo "Please contact hallo@uberspace.de so we can fix this. Thanks!"
+else
+  echo "Redis has been started."
 fi
 
-DAEMON=$0
-# Let's shift things up if we decide to allow multiple deletions:
-# shift
+# famous last words
+echo "Everything's been setup. You can use Redis now."
+echo
+echo "Please note that Redis does not use TCP."
+echo "Instead, we setup Redis to use a UNIX-Socket to connect to."
+echo
+echo "To connect to redis via socket, use:"
+echo
+echo "redis-cli ~/.redis/sock"
+echo 
 
-if [ "$DAEMON" = "" ] ; then
-  usage
-fi
-
-# There's a nasty syntax-error somewhere, nothing to see here...
-if [[ ! "$DAEMON" =~ "^[a-z]" ]]
-  usage
-fi
-
-if [ -d ~/service ] ; then
-  echo "~/service directory exists, good..."
-else 
-  echo "You don't seem to have any services in ~/service." 
-  exit 1
-fi
-
-# Let's see if this daemon actually exists
-if [ ! -f ~/service/$DAEMON ] ; then
-  echo "The daemon $DAEMON doesn't exist. "
-  exit 2
-fi 
-
-# Behold!
-echo "Deleting $DAEMON service"
-cd ~/service/$DAEMON
-rm ~/service/$DAEMON
-svc -dx . log
-rm -rf ~/etc/run-$DAEMON
-cd ~
-
-exit 0
