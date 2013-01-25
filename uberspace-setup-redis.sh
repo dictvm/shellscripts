0015ab6925e825153d26bb31d8d5063d02910ea7
Daniel Heitmann <dictvm@lynx.uberspace.de>
2013-01-25 12:08:17 +0100

    Fixed a few bugs Jonas found.

--- uberspace-setup-redis.sh
+++ uberspace-setup-redis.sh
@@ -1,63 +1,76 @@
-#!/bin/sh
+#!/bin/bash
 
-USER=`id -u`
+MYUID=`id -u`
 
-if [ "$USER" = "0" ] ; then
+if [ "$MYUID" = "0" ] ; then
   echo "This script is not meant to be run as root."
   exit 1
 fi
 
 if [ ! -d ~/service ] ; then
   echo "You don't have a ~/service directory."
-  echo "Please run \"uberspace-setup-svscan\" to setup daemon-tools before setting up Redis."
-  exit 2
+  echo "Please run \"uberspace-setup-svscan\" first to initialize daemon-tools before setting up Redis."
+  exit 1
 fi
 
 for DIR in ~/.redis ~/service/redis ~/etc/run-redis ; do
   if [ -d $DIR ] ; then
     echo "You already have a $DIR directory. Nothing to do here."
-    exit 3
+    exit 1
   fi
 done
 
-# Let's build redis
-echo "Installing redis. This might take some time."
-exec toast arm redis
-
-if ["$?" ! = "0" ] then
-  echo "Redis-installation with toast failed. This should not have happend."
-  echo "Please contact hallo@uberspace.de so we can fix this. Thanks!"
-  echo "Redis could not be installed. We're really sorry."
-  exit 3
+# We no longer need to build redis. It's preinstalled on all hosts.
+#echo "Installing redis. This might take some time."
+#toast arm redis
 
 # create configuration for redis
 echo "Creating Redis-configuration"
-mkdir .redis
-cat <<__EOF__ > unixsocket /home/$USER/.redis/sock
+mkdir ~/.redis
+cat <<__EOF__ > ~/.redis/conf
+unixsocket /home/$USER/.redis/sock
 daemonize no
-logfile /home/$USER/.redis/log
+logfile stdout
 port 0
 __EOF__
 
 mkdir ~/etc/run-redis
 cat <<__EOF__ > ~/etc/run-redis/run
 #!/bin/sh
-exec redis-server ~/.redis/conf
+exec redis-server ~/.redis/conf 2>&1
 __EOF__
 chmod 755 ~/etc/run-redis/run
 
+# Create the logging-script for redis/daemon-tools
+echo "Creating the ~/etc/run-redis/log/run logging run script"
+mkdir ~/etc/run-redis/log
+cat <<__EOF__ > ~/etc/run-redis/log/run
+#!/bin/sh
+exec multilog t ./main
+__EOF__
+chmod 755 ~/etc/run-redis/log/run
+
 echo "Creating symlink for ~/etc/run-redis to ~/service/redis to start the service"
-exec ln -s ../etc/run-redis ~/service/redis
+ln -s ../etc/run-redis ~/service/redis
 
-echo "Starting Redis..."
-svc -u ~/service/redis
+# Check if redis-service starts sucessfully
+WAIT=0
+echo -n "Waiting for Redis to start..."
+while [ $WAIT -lt 6 ]; do
+  WAIT=$(($WAIT+1))
+  echo -n "."
+  svok ~/service/redis 2>/dev/null
+  SVOK=$?
+  if [ "$SVOK" = "0" ] ; then
+    break
+  fi
+  sleep 1
+done
 
-if [ "$?" ! = "0" ] 
-then
-  echo "Redis couldn't be started. This should not have happened."
-  echo "Please contact hallo@uberspace.de so we can fix this. Thanks!"
-else
-  echo "Redis has been started."
+if [ "$SVOK" != "0" ] ; then
+  echo " failed."
+  echo "We're really sorry; this shouldn't have happened - please contact hallo@uberspace.de for help. Thanks a lot!"
+  exit 4
 fi
 
 # famous last words
@@ -68,6 +81,5 @@ echo "Instead, we setup Redis to use a UNIX-Socket to connect to."
 echo
 echo "To connect to redis via socket, use:"
 echo
-echo "redis-cli ~/.redis/sock"
+echo "redis-cli -s ~/.redis/sock"
 echo 
-
