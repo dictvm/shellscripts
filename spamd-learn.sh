aaf926a4ae01c2371d72c1022dd351baa1563cde
Daniel Heitmann <dictvm@lynx.uberspace.de>
2014-03-31 14:55:18 +0200

    added todo

--- spamd-learn.sh
+++ spamd-learn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # update spamd with spam/ham
+# todo: make it univeral for all vusers
 sa-learn --no-sync --spam ~/users/dh/.Spam/cur/
 sa-learn --no-sync --spam ~/users/dh/.Trash/cur/
 sa-learn --no-sync --ham ~/users/dh/cur/*
