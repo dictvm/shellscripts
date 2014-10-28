ad181345124e14754e51f4b1e1fe19e54d2a11db
Daniel Heitmann <dictvm@horrendum.de>
2014-10-28 13:56:03 +0100

    version-bump to 0.5.3

--- update-ghost.sh
+++ update-ghost.sh
@@ -4,7 +4,7 @@
 # the documentation of the Uberspace-wiki: http://goo.gl/eW5TlR
 
 # set the current ghost-version here
-VERSION='0.5.2'
+VERSION='0.5.3'
 
 # let's save the current CentOS-version
 RHEL='cat /etc/redhat-release | cut -d" " -f3 | cut -d "." -f1"'
