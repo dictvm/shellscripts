a1f22fd5838f11d0d96c4c501aaa6a96b4052624
Daniel Heitmann <dictvm@horrendum.de>
2015-11-14 02:16:01 +0100

    there's no need for this anymore.

--- spamd-learn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# update spamd with spam/ham
-# todo: make it univeral for all vusers
-sa-learn --no-sync --spam ~/users/dh/.Spam/cur/
-sa-learn --no-sync --ham ~/users/dh/cur/
-sa-learn --sync
